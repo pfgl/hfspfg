bad6a030ad0cb14336aef6967062f88165ea237d
jackmachin <jack.machin125@gmail.com>
2015-10-02 15:26:48 +0100

    DASgj

---
+++ footer.php
@@ -0,0 +1,93 @@
+	</div><!--body-->
+
+</div><!--container-->
+
+<div id="extended-footer-wrap">
+
+	<div id="extended-footer">
+		<div id="contact">
+			<h3>Contact Us</h3>
+
+			<address>
+				<p>Harrogate Financial Solutions Limited<br />
+				York House<br />
+				Cottingley Business Park<br />
+				Bradford<br />
+				West Yorkshire<br />
+				BD16 1PE<br />
+			</address>
+
+			<ul id="contact-details">
+				<li id="telephone">01423 873 757</li>
+				<li id="email"><a href="mailto:info@hfspfg.co.uk">Send us an E-mail</a></li>
+
+			</ul>
+		</div><!--contact-->
+
+		<div id="footernav">
+		</div><!--footernav-->
+
+		<div id="pfg-news">
+			<h3>News</h3>
+
+			<ul id="news-items">
+				<?php query_posts('posts_per_page=2'); ?>
+				<?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+					<li>
+						<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+						<?php the_excerpt(); ?>
+					</li>
+				<?php endwhile; ?>
+				<?php endif; ?>
+				<?php wp_reset_query(); ?>
+			</ul>
+
+			<p><a href="/hfspfg/news" class="more">View more news items &raquo;</a></p>
+		</div>
+
+		<div id="happy-clients">
+			<?php query_posts('posts_per_page=1&cat=3'); ?>
+			<?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+				<blockquote>
+					<?php the_content(); ?>
+				</blockquote>
+			<?php endwhile; ?>
+			<?php endif; ?>
+			<?php wp_reset_query(); ?>
+		</div>
+	</div>
+
+	<div id="subsidary-statement">
+		<a href="http://www.pfgl.co.uk"><img src="<?php bloginfo('template_url'); ?>/images/perspective_logo_small.png" alt="Perspective Financial Group Ltd Logo" /></a>
+		<p><?php bloginfo('name'); ?> is a subsidiary of Perspective Financial Group Limited.</p>
+	</div>
+</div>
+
+<div id="footer">
+		<p><small>Registered in England & Wales No: 4021420.  Registered Office: Paradigm House, Brooke Court, Wilmslow, Cheshire, SK9 3ND.</small></p>
+		<p><small><?php bloginfo('name'); ?> is a subsidiary of Perspective Financial Group Limited.</small></p>
+		<p><small><?php bloginfo('name'); ?> is authorised and regulated by the Financial Conduct Authority.</small></p>
+
+
+   <p><small>Should you have cause to complain, and you are not satisfied with our response to your complaint, you may be able to refer it to the Financial Ombudsman Service, which can be contacted as follows:</small></p>
+   <p><small>The Financial Ombudsman Service Exchange Tower, London, E14 9SR</small></p>
+   <p><small>Tel: 0800 023 4567 or 0300 123 9 123</small></p>
+ <p><small><a href="http://www.financial-ombudsman.org.uk">www.financial-ombudsman.org.uk</a></small></p>
+		<ul class="footer-links">
+			<li><a href="/sitemap.xml">Sitemap</a> | </li>
+			<li><a href="/find-us">Find Us</a></li>
+		</ul>
+
+		<ul>
+			<li>&copy; <?php bloginfo('name'); ?> <?php echo date('Y'); ?>. All rights reserved. | </li>
+
+		</ul>
+</div><!--footer-->
+
+<?php wp_footer(); ?>
+
+<div class="cookie_layer">
+            <div class="cookie_inner"><span class="cookie_icon">&nbsp;</span><div class="cookie_text">This website uses analytical cookies. By continuing to browse the site, you are agreeing to our use of cookies. To find out more about the analytical cookies we use and how to switch them off please follow this <a href="/privacy-policy">link</a>.</div></div>
+        </div>
+</body>
+</html>
---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+// On load
+
+// Fade Hyperlinks
+
+$(document).ready(function() {
+	$('#content a:link, .menu a').dwFadingLinks({
+		color: '#a40d36',
+		duration: 700
+	});
+
+	// Add PDF icon to downloads
+	$("a[href$='.pdf']").addClass("pdf-download");
+
+	// Home Page Slider
+	$('#slider').nivoSlider({
+		effect: 'sliceDown',
+		pauseTime: 6000,
+		directionNav: false,
+		controlNav: true,
+		pauseOnHover: false
+	});
+
+
+	// Footer Happy Clients
+	$('#happy-clients').cycle ({
+		fx:  'fade',
+		timeout: 12000,
+		speed: 'slow',
+		pause: 1
+	});
+
+	// blinking alert icon
+	$("#alert-image").cyclicFade({
+		repeat: 0,
+		params: [{fadeout:250, stayout:500, opout:0, fadein:250, stayin:1000, opin:1}]
+	});
+
+	// animate banner images
+	$('.banner').hover(function() { //On hover...
+
+		var thumbOver = $(this).find('img').attr('src'); //Get image url and assign it to 'thumbOver'
+
+		//Set a background image(thumbOver) on the <a> tag - Set position to bottom
+		$(this).css({'background' : 'url(' + thumbOver + ') no-repeat center bottom'});
+
+		//Animate the image to 0 opacity (fade it out)
+		$(this).find('img').stop().fadeTo('normal', 0 , function() {
+			$(this).hide(); //Hide the image after fade
+		});
+	} , function() { //on hover out...
+		//Fade the image to full opacity
+		$(this).find('img').stop().fadeTo('normal', 1).show();
+	});
+
+
+	// Same Page Link Scrolling
+    $('a[href*=#]').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+        && location.hostname == this.hostname) {
+            var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+                var targetOffset = $target.offset().top;
+                $('html,body').animate({scrollTop: targetOffset}, 1000);
+                return false;
+            }
+        }
+    });
+});
